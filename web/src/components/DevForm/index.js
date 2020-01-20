100d637cc8b444b7cb828d131a2a74c81b76820f
Flavio Rocha <fmrocha@gmail.com>
2020-01-19 16:02:51 -0800

    fixed untracked files

--- web/src/components/DevForm/index.js
+++ web/src/components/DevForm/index.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect }  from 'react'
-import './styles.css';
+import React, { useState, useEffect }  from 'react';
 
 function DevForm({ onSubmit }) {
     const [github_username, setGithubUsername] = useState('');
